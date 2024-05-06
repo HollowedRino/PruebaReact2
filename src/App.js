ee59ee084f4c5a2afb79c83ff8f8489eba656080
H04326 <H04326@lab.aca>
2024-05-06 14:34:29 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MiBoton(){
+  return(
+    <button type='button'>Hacer click</button>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MiBoton/>
+      <MiBoton/>
+    </>
   );
 }
 
